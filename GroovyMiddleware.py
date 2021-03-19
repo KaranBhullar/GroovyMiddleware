59f09597566686a6c8af2f4eae29b3907a479fbc
KaranBhullar <cswailord@gmail.com>
2021-03-19 02:53:25 -0400

    code added

--- GroovyMiddleware.py
+++ GroovyMiddleware.py
@@ -1,2 +1,20 @@
 import spotipy;
+from spotipy.oauth2 import SpotifyClientCredentials
 import discord;
+import os;
+
+clientID = os.environ.get('SPOTIFY_GROOVYMIDDLEWARE_CLIENT_PUBLIC');
+clientPrivate = os.environ.get('SPOTIFY_GROOVYMIDDLEWARE_CLIENT_PRIVATE');
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials());
+
+class MyClient(discord.Client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+			
+
+client = MyClient();
+client.run(os.environ.get('DISCORD_GROOVYMIDDLEWARE_TOKEN'));
